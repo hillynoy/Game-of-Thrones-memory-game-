42de88f2370770fc35740be391ad74e0bc24be91
hilly noy <hilly88@gmail.com>
2016-06-29 18:45:02 +0300

    newest

--- JS/memory_game.js
+++ JS/memory_game.js
@@ -1,3 +1,7 @@
+//global variables
+var card1 = new Image();
+var card2 = new Image();
+
 var allCards = document.getElementsByClassName('card');
 
 //creating an array of the hidden images so I can shuffle them before assigning them to each card
@@ -11,18 +15,18 @@ var createImage = function(src, title) {
 
 var imageArr = [];
 
-imageArr[0] = createImage("./PICS/arya.jpg", 'Image1');
-imageArr[1] = createImage("./PICS/calisi.jpg", 'Image2');
-imageArr[2] = createImage("./PICS/drogo.jpg", 'Image3');
-imageArr[3] = createImage("./PICS/jofry.jpg", 'Image4');
-imageArr[4] = createImage("./PICS/jon.jpg", 'Image5');
-imageArr[5] = createImage("./PICS/sansa.jpg", 'Image6');
-imageArr[6] = createImage("./PICS/arya.jpg", 'Image1');
-imageArr[7] = createImage("./PICS/calisi.jpg", 'Image2');
-imageArr[8] = createImage("./PICS/drogo.jpg", 'Image3');
-imageArr[9] = createImage("./PICS/jofry.jpg", 'Image4');
-imageArr[10] = createImage("./PICS/jon.jpg", 'Image5');
-imageArr[11] = createImage("./PICS/sansa.jpg", 'Image6');
+imageArr[0] = createImage("./PICS/c_arya.jpg", 'Image1');
+imageArr[1] = createImage("./PICS/c_cersi.jpg", 'Image2');
+imageArr[2] = createImage("./PICS/c_drogo.jpg", 'Image3');
+imageArr[3] = createImage("./PICS/c_joffry.jpg", 'Image4');
+imageArr[4] = createImage("./PICS/c_snow.jpg", 'Image5');
+imageArr[5] = createImage("./PICS/c_merg.jpg", 'Image6');
+imageArr[6] = createImage("./PICS/c_arya.jpg", 'Image1');
+imageArr[7] = createImage("./PICS/c_cersi.jpg", 'Image2');
+imageArr[8] = createImage("./PICS/c_drogo.jpg", 'Image3');
+imageArr[9] = createImage("./PICS/c_joffry.jpg", 'Image4');
+imageArr[10] = createImage("./PICS/c_snow.jpg", 'Image5');
+imageArr[11] = createImage("./PICS/c_merg.jpg", 'Image6');
 
 // shuffling the images on every new round
 function shuffle(a) {
@@ -43,20 +47,41 @@ for (var i = 0; i < allCards.length; i++) {
     allCards[i].appendChild(imageArr[i]);
 }
 
-//function flip a clicked card
+//function flip a clicked card and store its source
+    var flipCard = function (click) {
+        var card = click.target;
+        var flipped_img = card.getElementsByTagName('img')[0];
+        console.log(flipped_img);//creating an array of the images
+        flipped_img.style.display = "block";
+                         //setting the display of images from hidden to shown
 
-var flipCard = function(click) {
-    var card = click.target;
-    var img = card.getElementsByTagName('img')[0];             //creating an array of the images
+        if (card1.src === "") {
+            card1 = flipped_img;                          //check if the first global cardSrc is occupied with a previous code, if so- store the new image src in "card2Src"
+            console.log("this is card 1 ", card1.src);
+        }else{
+            card2 = flipped_img;
+            console.log("this is card 2 " ,card2.src);
+            //todo check if both have the same image src
+            if (card1.src !== card2.src) {
+                console.log("i'm in!!!");
+                setTimeout(function(){card1.style.display = "none";card1 = new Image();}, 1000);
+                setTimeout(function(){card2.style.display = "none";card2 = new Image();}, 1000);
 
-        img.style.display = "block";                //setting the display of images from hidden to shown
-    }
+
+            }
+        //     //else just leave the cards display:block
+        }
+};
 
 
 var clickedCard = document.getElementsByClassName("card");      //setting every card the click functionality that sends it to the flip function.
 for (var i = 0; i < clickedCard.length; i++) {
     clickedCard[i].addEventListener("click", flipCard);
 }
+
+
+
+
 
 
 
---
+++ test_memory.js
@@ -0,0 +1,5 @@
+/**
+ * Created by itc_user on 6/29/2016.
+ */
+img = new Image()
+img.src
