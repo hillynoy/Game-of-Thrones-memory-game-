62562ef09d9736b4de6a646409955ddea512dd5e
hilly noy <hilly88@gmail.com>
2016-07-02 23:01:22 +0300

    +closed winning mes. todo- reset and stop when winning todo- glow cards on hover

--- JS/memory_game.js
+++ JS/memory_game.js
@@ -16,12 +16,10 @@ var createBoard = function (imgArray) {
     }
     doubleArray = shuffle(doubleArray);
     board.innerHTML = "";
-    // var counter = 0;
     for (var i = 0; i < doubleArray.length; i++) {
         var card = document.createElement('div');
         card.className = "card";
         card.addEventListener("click", flipCard);
-        // card.addEventListener("mouseover",lightCard);
         var img = createImage(doubleArray[i]);
         card.appendChild(img);
         board.appendChild(card);
@@ -35,7 +33,7 @@ var startAdvancedGame = function () {
     createBoard(imgArray);
 };
 
-var startMediumGame = function () {
+var startMediumGame = function (click) {
     medGameActive = true;
     var imgArray = ["arya1.jpg", "cersi1.jpg", "drogo1.jpg", "joffry1.jpg", "snow1.jpg", "merg1.jpg", "calisi1.jpg", "half1.jpg", "briene1.jpg"];
     createBoard(imgArray);
@@ -75,18 +73,14 @@ var flipCard = function (click) {
         flipped_img.style.display = "block";
 
         //setting the display of images from hidden to shown
-
         if (card1.src === "") {
             card1 = flipped_img;                          //check if the first global cardSrc is occupied with a previous code, if so- store the new image src in "card2Src"
-
         }
         else {
             card2 = flipped_img;
             //check if both have the same image src
             if (card1.src !== card2.src) {
-
                 gameIsOn = false;
-
                 setTimeout(function () {
                     card1.style.display = "none";
                     card1 = new Image();
@@ -106,16 +100,12 @@ var flipCard = function (click) {
             count = 0;
             easyGameActive = false;
             youHaveWon();
-            // /* Stop button */
-            // stop.onclick = function() {
-            //     clearTimeout(t);
-                }
+        }
 
         if ((medGameActive === true) && (count === 18)) {
             count = 0;
             medGameActive = false;
             youHaveWon();
-
         }
         if ((advGameActive === true) && (count === 24)) {
             count = 0;
@@ -142,6 +132,8 @@ var chooseLevel = function () {
     medBtn.textContent = "Wildling brain";
     medBtn.className = "btn";
     medBtn.addEventListener('click', startMediumGame, timer);
+    console.log("im in med");
+
     title.appendChild(medBtn);
 
     var advBtn = document.createElement('button');
@@ -152,27 +144,20 @@ var chooseLevel = function () {
 };
 
 var youHaveWon = function() {
-setTimeout(function () {
-    var msg = "The Iron Throne is yours!";
-    // var  bg = "url('../PICS/thr.jpg')";
+    setTimeout(function () {
+        var msg = "The Iron Throne is yours!";
 
-    document.getElementById("message").innerHTML = msg;
-    // document.getElementById("message").style.backgroundImage = "url('../PICS/thr.jpg')";
-    document.getElementById("lightbox").style.display = "block";
-    document.getElementById("lightbox").style.backgroundImage = "url('../PICS/thr.jpg')";
-}, 100);
+        document.getElementById("message").innerHTML = msg;
+        document.getElementById("lightbox").style.display = "block";
+        document.getElementById("lightbox").style.backgroundImage = "url('../PICS/thr.jpg')";
+    }, 100);
 
     setTimeout(function () {
         var msg = "";
-        // var  bg = "url('../PICS/thr.jpg')";
-
         document.getElementById("message").innerHTML = msg;
-        // document.getElementById("message").style.backgroundImage = "url('../PICS/thr.jpg')";
         document.getElementById("lightbox").style.display = "none";
-        // document.getElementById("lightbox").style.backgroundImage = "url('../PICS/thr.jpg')";
     }, 4000);
 };
-
 
 chooseLevel();
 
@@ -181,13 +166,11 @@ chooseLevel();
 
 
 var h1 = document.getElementsByTagName('h1')[0],
-    start = document.getElementsByClassName('btn')[0],
-
-    stop = document.getElementById('stop'),
+    start  = document.getElementsByTagName('button')[0],
+    stop = document.getElementsByClassName('btn'),
     clear = document.getElementById('clear'),
     seconds = 0, minutes = 0, hours = 0,
     t;
-
 
 function add() {
     seconds++;
@@ -199,31 +182,28 @@ function add() {
             hours++;
         }
     }
-    h1.textContent = 
-            (hours ? (hours > 9 ? hours : "0" + hours) 
-            : "00") + ":" + (minutes ? (minutes > 9 ? minutes 
+    h1.textContent =
+        (hours ? (hours > 9 ? hours : "0" + hours)
+            : "00") + ":" + (minutes ? (minutes > 9 ? minutes
             : "0" + minutes) : "00") + ":" +
-            (seconds > 9 ? seconds : "0" + seconds);
+        (seconds > 9 ? seconds : "0" + seconds);
     timer();
 }
-
 
 function timer(click) {
     t = setTimeout(add, 1000);
 }
 
-
 /* Start button */
 start.onclick = timer;
 
 /* Stop button */
-stop.onclick = function() {
-
+stop.click = function() {
     clearTimeout(t);
 };
 
 /* Clear button */
-clear.onclick = function() {
+clear.click = function() {
     h1.textContent = "00:00:00";
     seconds = 0; minutes = 0; hours = 0;
 };
